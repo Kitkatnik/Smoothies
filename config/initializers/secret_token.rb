874f1d5201ff5f0f8a9525b5bc08e1d2aab23d6a
Katya Sarmiento <Kat@Katyas-MacBook-Pro.local>
2013-11-06 10:49:30 -0500

    Created rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smoothie::Application.config.secret_key_base = '9a18445043236b8a62b5781e612a163c8febb4f2c3725d152c8f08aecf47f8852053d66469c24317ee46941c26e5bd3e210876ecc65bbc0b876b4d02407c4375'
